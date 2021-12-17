02475c4d63e11a47443a1bab1a68101d93432be8
NaRae-tech <narae.dev@gmail.com>
2021-12-17 15:13:26 +0900

    addeventListener 실행 순서 조정

--- src/components/getInputs.js
+++ src/components/getInputs.js
@@ -5,16 +5,14 @@ import { MakeToCar } from './makeToCar.js';
 import { PlayRacing } from './playRacing.js';
 
 function carNamesGet() {
-  let carNames = [];
   $('#car-names-submit').addEventListener('click', () => {
     const names = $('#car-names-input').value.split(',');
     if (CarNamesValidity(names)) {
       $('#car-names-submit').disabled = true;
       $('#racing-count-submit').disabled = false;
-      carNames = names;
+      tryCountGet(names);
     }
   });
-  return carNames;
 }
 function tryCountGet(carNamesList) {
   $('#racing-count-submit').addEventListener('click', () => {
@@ -27,6 +25,5 @@ function tryCountGet(carNamesList) {
   });
 }
 export function GetInputs() {
-  const carNamesList = carNamesGet();
-  tryCountGet(carNamesList); 
+  carNamesGet();
 }
