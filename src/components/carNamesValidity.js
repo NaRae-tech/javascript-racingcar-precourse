2fc87df697dec62aac0e27dcdb759280184b7ac7
NaRae-tech <narae.dev@gmail.com>
2021-12-17 13:21:08 +0900

    차 이름 유효성 체크

---
+++ src/components/carNamesValidity.js
@@ -0,0 +1,24 @@
+function lengthValidity(names) {
+  for (let i = 0; i < names.length; i++){
+    if (names[i].length > 5 || names[i].length<1) {
+      return alert('차의 이름은 1자 이상 5자 이하여야 합니다.');
+    }
+  }
+  return true;
+}
+function duplicationValidity(names) {
+  const setNames = new Set(names);
+  return (names.length === setNames.size) ? true : false;
+}
+
+export function CarNamesValidity(userInput) {
+  const names = userInput.split(',');
+  if (!lengthValidity(names)) {
+    return false;
+  }
+  if (!duplicationValidity(names)) {
+    return false;
+  }
+  console.log(names);
+  return true;
+}
