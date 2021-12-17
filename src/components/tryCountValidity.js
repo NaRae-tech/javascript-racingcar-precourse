65f30f72afe3548aaeb29b3a05961b02d5d494c3
NaRae-tech <narae.dev@gmail.com>
2021-12-17 13:28:03 +0900

    시도 횟수 유효성체크

---
+++ src/components/tryCountValidity.js
@@ -0,0 +1,21 @@
+function integerValidity(input) {
+  if (isNaN(parseInt(input))) {
+    return alert('숫자를 입력해주세요.');
+  }
+  return true;
+}
+function rangeValiditiy(input) {
+  if (parseInt(input) < 1) {
+    return alert('1이상의 숫자를 입력해주세요.');
+  }
+  return true;
+}
+export function tryCountValidity(countInput) {
+  if (!integerValidity(countInput)) {
+    return false;
+  }
+  if (!rangeValiditiy(countInput)) {
+    return false;
+  }
+  return true;
+} 
